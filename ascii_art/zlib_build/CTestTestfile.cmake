4fc00220fa287126b019ef8ea922b33da87fda54
Terminator15474 <jandommert8@gmail.com>
2022-11-13 16:57:59 +0100

    Automated commit on 13.11.2022

---
+++ ascii_art/zlib_build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: A:/Programieren/ascii_art/zlib
+# Build directory: A:/Programieren/ascii_art/zlib_build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(example "example")
+set_tests_properties(example PROPERTIES  _BACKTRACE_TRIPLES "A:/Programieren/ascii_art/zlib/CMakeLists.txt;199;add_test;A:/Programieren/ascii_art/zlib/CMakeLists.txt;0;")
